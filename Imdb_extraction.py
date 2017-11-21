1c6e02149359999fbd594732b1d25d08eb071c82
farshid <farshidrayhan57@gmail.com>
2017-11-21 21:13:51 +0600

    2nd commit

--- Imdb_extraction.py
+++ Imdb_extraction.py
@@ -1,1 +1,12 @@
-import imdb
+from imdb import IMDb
+ia = IMDb()
+
+# get a movie and print its director(s)
+the_matrix = ia.get_movie('0133093')
+# print(the_matrix['director'])
+print(the_matrix['runtimes'][0])
+print(the_matrix['year'])
+
+
+# show all the information sets avaiable for Movie objects
+# print(ia.get_movie_infoset())
